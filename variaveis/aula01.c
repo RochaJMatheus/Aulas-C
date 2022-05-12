e33201e1ef8595f0dfd2baab458fa64f51c81467
RochaJMatheus <matheus_rocha12@hotmail.com>
2022-05-11 21:29:05 -0300

    Aula01
    Aula Variáveis

---
+++ variaveis/aula01.c
@@ -0,0 +1,16 @@
+//Aula sobre variáveis
+#include <stdio.h>
+
+int main() {//Chave delimita um bloco de código (INICIO DO BLOCO)
+	//Declarando variáveis
+	int idade = 0 ;
+	
+	printf("Qual aé a sua idade ? \n");
+	//Reeber dados 
+	scanf("%d",&idade);
+	printf("A sua idade é %d ",idade);
+	printf("\n");
+	return 0;
+}//(FIM DO BLOCO)
+
+
